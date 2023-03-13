b65d4cdfc3e976f7dedc1ab173b676c9899c2059
Mohammed Jasir <jasirm203@gmail.com>
2023-03-13 19:26:27 +0530

    ui completed

--- lib/Controllers/group chat bloc/groupchat_bloc.dart
+++ lib/Controllers/group chat bloc/groupchat_bloc.dart
@@ -1,13 +1,11 @@
-import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'groupchat_event.dart';
 part 'groupchat_state.dart';
 
 class GroupchatBloc extends Bloc<GroupchatEvent, GroupchatState> {
   GroupchatBloc() : super(GroupchatInitial()) {
-    on<GroupchatEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+    on<GroupchatEvent>((event, emit) {});
   }
 }
